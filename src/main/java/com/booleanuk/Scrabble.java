33822672c22b975a1d97226e0b6c37fb87fb6806
Iris Flystam <badde00@gmail.com>
2024-08-13 11:35:57 +0200

    Gave up on extended points due to lack of time

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,39 @@
 package com.booleanuk;
 
-public class Scrabble {
-    public Scrabble(String word) {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class Scrabble {
+    private final String word;
+
+    public Scrabble(String word) {
+        this.word = word.toLowerCase();
     }
 
     public int score() {
-        return -1;
+        int score = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            int letterScore = letterScore(word.charAt(i));
+            score += letterScore;
+        }
+
+
+        return score;
+    }
+
+    private int letterScore(char letter) {
+        String[] letters = {"aeioulnrst", "dg", "bcmp", "fhvwy", "k", "jx", "qz"};
+        int[] points = {1, 2, 3, 4, 5, 8, 10};
+        int point = 0;
+
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i].contains("" + letter)) {
+                point = points[i];
+            }
+        }
+
+        return point;
     }
 
 }
